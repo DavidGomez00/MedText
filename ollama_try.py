72b51f959364179673a08dae8b07772ea80e603f
David Gómez <dvdgmzrtz@gmail.com>
2025-11-19 15:14:20 +0100

    Update repo

---
+++ ollama_try.py
@@ -0,0 +1,20 @@
+from langchain_ollama import OllamaLLM
+import os
+import dotenv
+import requests
+
+messages = [
+    {"role": "system", "content": "Eres un asistente especializado en dar los buenos días."},
+    {"role": "user", "content": "Buenos días."}
+]
+
+answer = requests.post(
+    url=f"http://176.98.223.168:11434/api/chat",
+    json={
+        "model": "llama3.1:8b",
+        "messages": messages,
+        "stream": False
+    }
+).json()
+
+print(answer)
